07fceb2791d41d75e7fc21aa5231c2e4d1f2257b
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-19 12:06:51 -0300

    Create global interface to all interfaces

--- api/src/interfaces/IUser.ts
+++
@@ -1,5 +0,0 @@
-export default interface User {
-    name: string;
-    email: string;
-    password: string;
-}
---
+++ api/src/interfaces/global.interface.ts
@@ -0,0 +1,41 @@
+import { Document, Types } from 'mongoose';
+
+interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    celular: number;
+    instruments: string[];
+    role: string;
+}
+
+interface IPost extends Document {
+    title: string;
+    description: string;
+    content: string;
+    author: Types.ObjectId;
+    category: string;
+    tags: string[];
+    createdAt: Date;
+    type: string;
+    visibility: string;
+    files: string[];
+}
+
+interface ISchedule extends Document {
+    title: string;
+    date: Date;
+    service: string;
+    members: Types.ObjectId[];
+    songs: Types.ObjectId[];
+}
+
+interface IMessage extends Document {
+    title: string;
+    content: string;
+    author: Types.ObjectId;
+    sentAt: Date;
+    recipients: Types.ObjectId[];
+}
+
+export { IUser, IPost, ISchedule, IMessage }
