21fee13fcf4a9a387e48eff877b6282650bac934
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-19 16:08:04 -0300

    Instal swagger ui and reate swagger.ts

---
+++ api/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'We Praise API',
+      version: '1.0.0',
+      description: 'An API to We Praise Project',
+    },
+  },
+  apis: ['./src/router.ts','./src/routes/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
