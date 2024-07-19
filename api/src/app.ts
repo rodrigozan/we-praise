e00613718a519c5e0f5c48ffa190d3226f99c414
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-19 16:11:38 -0300

    Import swagger.ts in app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,31 +1,37 @@
-import express from "express";
-import * as dotenv from 'dotenv';
+import express from "express"
+import { setupSwagger } from './swagger'
+import * as dotenv from 'dotenv'
 
-import { Connection } from './database/Connection';
+import { Connection } from './database/Connection'
 
-import router from "./router";
+import router from "./router"
 
 dotenv.config()
 
 export class App {
-    public server: express.Application;
+    public server: express.Application
 
     constructor() {
-        this.server = express();
-        this.middleware();
-        this.connection();
-        this.routes();
+        this.server = express()
+        this.middleware()
+        this.connection()
+        this.routes()
+        this.documentation()
     }
 
     private middleware() {
-        this.server.use(express.json());
+        this.server.use(express.json())
     }
 
     private routes() {
-        this.server.use('/api', router);
+        this.server.use('/api', router)
+    }
+
+    private documentation() {
+        setupSwagger(express())
     }
 
     private async connection() {
-        await Connection.connect();
+        await Connection.connect()
     }
 }
