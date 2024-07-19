1ea63a26b6c8d0d82d01e747ccb63e3732958c4f
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-19 13:09:42 -0300

    Create validation post

--- api/src/helpers/validation.ts
+++ api/src/helpers/validation.ts
@@ -2,7 +2,7 @@ import { IUser } from '../interfaces/global.interface';
 
 class validation {
     public validatePostAuthor(user: IUser) {
-        if (user.role !== 'editor') {
+        if (user.role === 'user') {
             throw new Error('User does not have permission to publish posts.');
         }
         return true;
