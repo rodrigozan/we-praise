0616e49efe1d0387d2a52fc8602bc4efd979d345
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-23 10:50:29 -0300

    Update AuthRoutes to insert swagger ui comments

--- api/src/routes/AuthRoutes.ts
+++ api/src/routes/AuthRoutes.ts
@@ -5,6 +5,30 @@ import AuthController from '../controllers/AuthController'
 const router = Router();
 const authController = AuthController
 
+/**
+ * @openapi
+ * /api/auth:
+ *   post:
+ *     summary: Atentica o usuário
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Usuário deletado com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.post('/auth', authController.login)
 
 export default router;
