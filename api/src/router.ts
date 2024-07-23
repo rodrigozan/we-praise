7644d9d60bd9c85e2b7c5bdffb6b0f804b556668
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-23 14:36:27 -0300

    Update router.ts

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express'
 
-import userRoutes from './routes/UserRoutes';
-import authRoutes from './routes/AuthRoutes';
-import changePasswordRoutes from './routes/ChangePasswordRoutes';
-import postRoutes from './routes/PostRoutes';
+import userRoutes from './routes/UserRoutes'
+import authRoutes from './routes/AuthRoutes'
+import changePasswordRoutes from './routes/ChangePasswordRoutes'
+import postRoutes from './routes/PostRoutes'
+import scales from './routes/ScalesRoutes'
 
 const router = Router()
 
-router.use(userRoutes);
-router.use(authRoutes);
-router.use(changePasswordRoutes);
-router.use(postRoutes);
+router.use(userRoutes)
+router.use(authRoutes)
+router.use(changePasswordRoutes)
+router.use(postRoutes)
+router.use(scales)
 
 export default router
