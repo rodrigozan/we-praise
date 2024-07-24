8161cd6b1e8f6351aff259c41eb6e0d8ae7b05c7
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-24 12:02:28 -0300

    Add Comment CRUD to validationHelpers

--- api/src/helpers/validationHelpers.ts
+++ api/src/helpers/validationHelpers.ts
@@ -1,6 +1,6 @@
 import { UserService } from '../services/UserService';
 import ValidationHelper from './ValidationHelper';
-import { IPost, ISong, IScale, IMessage } from '../interfaces/global.interface';
+import { IPost, ISong, IScale, IMessage, IComment } from '../interfaces/global.interface';
 
 function createValidationHelper<T>(): ValidationHelper<T> {
     return new ValidationHelper<T>(UserService.findUserById.bind(UserService));
@@ -10,5 +10,6 @@ const postValidationHelper = createValidationHelper<IPost>();
 const songValidationHelper = createValidationHelper<ISong>();
 const scaleValidationHelper = createValidationHelper<IScale>();
 const messageValidationHelper = createValidationHelper<IMessage>();
+const commentValidationHelper = createValidationHelper<IComment>();
 
-export { postValidationHelper, songValidationHelper, scaleValidationHelper, messageValidationHelper };
+export { postValidationHelper, songValidationHelper, scaleValidationHelper, messageValidationHelper, commentValidationHelper };
