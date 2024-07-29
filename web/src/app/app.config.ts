73e5b43c1436c83c1e50e5ab56719a147615a1a8
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-29 08:20:12 -0300

    add authInterceptor and StorageService in config

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,14 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
+import { StorageService } from './Services/storage/storage.service';
 import { routes } from './app.routes';
+import { authInterceptor } from './Interceptors/auth.interceptor';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideClientHydration(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor])),
+    StorageService
   ]
 };
