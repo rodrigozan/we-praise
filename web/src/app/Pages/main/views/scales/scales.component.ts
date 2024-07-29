076b88f6943d85e8038e3be7f6f6266097fde284
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-29 09:36:24 -0300

    Create scales component

---
+++ web/src/app/Pages/main/views/scales/scales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scales',
+  standalone: true,
+  imports: [],
+  templateUrl: './scales.component.html',
+  styleUrl: './scales.component.scss'
+})
+export class ScalesComponent {
+
+}
