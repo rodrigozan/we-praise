8e50a8e2b6fd4c971f5004d91d8dac518e81fd92
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-29 09:39:40 -0300

    Update app.config.ts
    Add withFetch to privideHttpClient withInterceptors in app.config.ts

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 import { StorageService } from './Services/storage/storage.service';
 import { routes } from './app.routes';
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     StorageService
   ]
 };
