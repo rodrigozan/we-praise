ba57a0d6aadfff297cef1a5c434f563354f06f5b
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-29 08:23:28 -0300

    Create dictionary.ts

---
+++ web/src/app/Interfaces/dictionary.ts
@@ -0,0 +1,119 @@
+export class IListDictionary {
+
+    /* Titles */
+
+    loading = 'Carregando...'
+    loadingWait = 'Aguarde...'
+    loadingUpdate = 'Atualizando...'
+    loadingSend = 'Enviando...'
+    titleAPP = 'We Praise'
+    titleWelcome = 'Bem-vindo(a)'
+    titleTo = 'ao'
+    titleName = 'Nome'
+    titleEmail = 'Email'
+    titlePassword = 'Senha'
+    titleCPF = 'CPF'
+    titleZipCode = 'CEP'
+    titleAddress = 'Endereço'
+    titleStreet = 'Rua'
+    titleDistrict = 'Bairro'
+    titleCity = 'Cidade'
+    titleNumber = 'Número'
+    titleComplement = 'Complemento'
+    titleLocal = 'Local'
+    titleDateHours = 'Data / Hora'
+    titleHours = 'Hora'
+    titleDate = 'Data'
+    titleNew = 'Novo'
+    titleFilter = 'Filtrar'
+    titleAdvancedFilter = 'Filtro Avançado'
+    titleAttachments = 'Anexos'
+    titleAttach = 'Anexar'
+    titleForm = 'Formulário'
+    titleToView = 'Visualizar'
+    titleToInsert = 'Inserir'
+    titleToDelete = 'Excluir'
+    titleToUpdate = 'Atualizar'
+    titleEdit = 'Editar'
+    titleDuplicate = 'Duplicar'
+    titleRefuse = 'Recusar'
+    titleApprove = 'Aprovar'
+    titleAdd = 'Adicionar'
+    titleClear = 'Limpar'
+    titleComments = 'Comentário'
+    titleStatus = 'Status'
+    titleActive = 'Ativo'
+    titleActived = 'Ativado'
+    titleDisabled = 'Desativado'
+    titleActivate = 'Ativar'
+    titleDisable = 'Desativar'
+    titleAgain = 'Novamente'
+    titleUsername = 'Usuário'
+    titleUsernames = 'Usuários'
+    titleListing = 'Listagem'
+
+    titleScales = {
+        pt: 'Escalas',
+        en: 'Scales'
+    }
+    titlePosts = {
+        pt: 'Publicações',
+        en: 'Publications'
+    }
+    titleSongs = {
+        pt: 'Músicas',
+        en: 'Musics'
+    }
+    titleMessages = {
+        pt: 'Mensagens',
+        en: 'Messages'
+    }
+
+    /* Routes */
+    routeMain = 'main'
+
+    /* Buttons */
+
+    btnAccess = 'Acessar'
+    btnLogout = 'Sair'
+    btnConfirm = 'Confirmar'
+    btnAdvance = 'Avançar'
+    btnNext = 'Próximo'
+    btnPrevious = 'Anterior'
+    btnBack = 'Voltar'
+    btnCheckin = 'Check-In'
+    btnNot = 'Não'
+    btnYes = 'Sim'
+    btnUpdate = 'Atualizar'
+    btnSend = 'Enviar'
+    btnCancel = 'Cancelar'
+    btnClear = 'Limpar'
+    btnSave = 'Salvar'
+    btnDelete = 'Excluir'
+    btnKeepSignedIn = 'Mantenha-me conectado'
+    btnSignedIn = 'Conectar'
+
+    /* Toasts */
+
+    toastNotConnection = 'Não há conexão com a internet!'
+    toastConnection = 'Conectado há internet'
+    toastUsernameInvalid = 'Campo Usuário é obrigatório!'
+    toastPasswordInvalid = 'Campo Senha é obrigatório!'
+    toastLoginInvalid = 'Usuário ou senha está incorreto!'
+
+    /* Messages */
+
+    messsageTitleSuccess = 'Sucesso'
+    messsageTitleError = 'Erro'
+    messsageTitleWarning = 'Aviso'
+    messsageTitleInformation = 'Informação'
+    messageContentSuccess = '[ID] com sucesso!'
+    messageContentError = '[ID] erro'
+    messageContentAddSuccess = '[ID] criada com sucesso!'
+    messageContentEditSuccess = '[ID] alterada com sucesso!'
+    messageContentRemoveSuccess = '[ID] removido com sucesso!'
+    messageContentAddError = 'Erro ao criar [ID]!'
+    messageContentEditError = 'Erro ao alterar [ID]!'
+    messageContentRemoveError = 'Erro ao remover [ID]!'
+    
+}
