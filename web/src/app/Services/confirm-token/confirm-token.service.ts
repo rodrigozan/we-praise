134804dc16365827879921fc763b26193a66e915
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-29 09:30:19 -0300

    Create confirm-token.service.ts

---
+++ web/src/app/Services/confirm-token/confirm-token.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../Pages/auth/services/auth.service'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmTokenService {
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+  ) { }
+
+  isLogged(){
+    if(this.authService.isLoggedIn()){
+      this.router.navigate(['/main'])
+    }else {
+      this.router.navigate(['/login'])
+    }
+  }
+}
