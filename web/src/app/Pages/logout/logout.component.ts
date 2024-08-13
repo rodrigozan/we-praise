51f27e8d1c2d70cfbe642b8df8f1eebe224ce88a
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-08-13 15:39:30 -0300

    Created logout component

---
+++ web/src/app/Pages/logout/logout.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../Pages/auth/services/auth.service'
+
+import { IListDictionary } from '../../Interfaces/dictionary';
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss'
+})
+export class LogoutComponent implements OnInit {
+
+  dictionary: IListDictionary = new IListDictionary()
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+  ){}
+
+  ngOnInit(): void {
+      setTimeout(() => {
+        this.logout()
+      }, 2000);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
